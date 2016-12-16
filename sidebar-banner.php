2a2102e69d29bd20988ddb84107fdc5e15ffadd3
Fritz <fre.healy@gmail.com>
2016-12-16 13:35:52 -0500

    changing sidebar-banner.php

--- sidebar-banner.php
+++ sidebar-banner.php
@@ -4,26 +4,39 @@
  *
  */ ?>
 <div id="banner">
-	<?php $flexslider = get_field("flexslider");
-	if($flexslider):?>
+	<?php $flexslider = get_field( "flexslider_banner" );
+	if ( $flexslider ):?>
 		<div class="flexslider">
 			<ul class="slides">
-				<?php foreach($flexslider as $row):?>
-					<li>
-						<?php if(strcmp($row['video_or_image'],"video")===0&&$row['video']):?>
+				<?php foreach ( $flexslider as $row ): ?>
+					<?php if ( strcmp( $row['video_or_image'], "video" ) === 0 && $row['video'] ): ?>
+						<li>
 							<div class="iframe-wrapper">
-								<iframe src="<?php echo $row['video'];?>" allowfullscreen="true" frameborder="0"></iframe>
+								<iframe src="<?php echo $row['video']; ?>" allowfullscreen="true"
+								        frameborder="0"></iframe>
 							</div><!--.iframe-wrapper-->
-						<?php elseif(strcmp($row['video_or_image'],"image")===0&&$row['link']&&$row['image']): ?>
-							<a href="<?php echo $row['link'];?>" <?php if($row['target']):echo 'target="_blank"'; endif;?>>
-								<img src="<?php echo $row['image']['url'];?>" alt="<?php echo $row['image']['alt'];?>">
-							</a>
-						<?php elseif(strcmp($row['video_or_image'],"image")===0&&$row['image']): ?>
-							<img src="<?php echo $row['image']['url'];?>" alt="<?php echo $row['image']['alt'];?>">
-						<?php endif;?>
-					</li>
-				<?php endforeach;?>
+						</li>
+					<?php elseif ( strcmp( $row['video_or_image'], "image" ) === 0 && $row['link'] && $row['image'] ): ?>
+						<li>
+							<div class="image-wrapper"
+							     style="background-image: url(<?php echo $row['image']['url']; ?>);">
+								<a href="<?php echo $row['link']; ?>" <?php if ( $row['target'] ):echo 'target="_blank"'; endif; ?>>
+									<img src="<?php echo $row['image']['url']; ?>"
+									     alt="<?php echo $row['image']['alt']; ?>">
+								</a>
+							</div><!--.image-wrapper-->
+						</li>
+					<?php elseif ( strcmp( $row['video_or_image'], "image" ) === 0 && $row['image'] ): ?>
+						<li>
+							<div class="image-wrapper"
+							     style="background-image: url(<?php echo $row['image']['url']; ?>);">
+								<img src="<?php echo $row['image']['url']; ?>"
+								     alt="<?php echo $row['image']['alt']; ?>">
+							</div><!--.image-wrapper-->
+						</li>
+					<?php endif; ?>
+				<?php endforeach; ?>
 			</ul>
 		</div>
-	<?php endif;?>
+	<?php endif; ?>
 </div>
