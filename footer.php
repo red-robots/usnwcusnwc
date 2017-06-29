86d8a883d52803afcc428b28b73ded9c9e77b332
Fritz <fre.healy@gmail.com>
2017-06-29 15:16:39 -0400

    changing footer link

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@
 	<div class="row sponsors"> 
 		<ul>
    	        <li class="subaru"><a href="http://www.subaru.com/" target="_blank"><img src="/wp-content/uploads/2015/09/Subaru_Grey_156x25.png" alt="Subaru Logo"></a></li>
-           	<li class="parknrec"><a href="http://charmeck.org/mecklenburg/county/ParkandRec/Pages/default.aspx" target="_blank"><img src="/wp-content/uploads/2015/06/ParksRec_Grey_119x35.png" alt="Mecklenburg County Park and Recreation Logo"></a></li>
+           	<li class="parknrec"><a href="https://www.mecknc.gov/ParkandRec/Pages/Home.aspx" target="_blank"><img src="/wp-content/uploads/2015/06/ParksRec_Grey_119x35.png" alt="Mecklenburg County Park and Recreation Logo"></a></li>
 		</ul>
 	</div>
 	<div class="row search">
