7f06518b025fbd3688342e853340fdad10a85940
Fritz <fre.healy@gmail.com>
2018-01-19 11:01:03 -0500

    adding search page

---
+++ header-search.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<?php
+       wp_enqueue_script('jquery');
+       wp_enqueue_script('jquery-core-ui');
+       wp_enqueue_script('jquery-tabs-ui');
+       wp_enqueue_script('hoverlink', get_bloginfo('template_directory').'/rollover.js');
+    ?>
+
+<title><?php
+	/*
+	 * Print the <title> tag based on what is being viewed.
+	 */
+	wp_title( '|', true, 'right' );
+
+	// Add the blog name.
+	bloginfo( 'name' );
+?></title>
+<?php wp_head();?>
+<meta charset="<?php bloginfo( 'charset' ); ?>" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="google-site-verification" content="e3L8kU2GBIb6wsJkRj2n6v1GDc-ajo6yg6IgX2xYl7w" />
+<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+    <link rel="stylesheet" type="text/css"  href="<?php echo get_template_directory_uri(); ?>/style.css?v=<?php echo time();?>" />
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/flexslider.css?v=<?php echo time();?>" />
+<link rel="apple-touch-icon" sizes="57x57" href="/wp-content/uploads/2014/10/W_57x57.jpg" />
+<link rel="apple-touch-icon" sizes="72x72" href="/wp-content/uploads/2014/10/W_72x72.jpg" />
+<link rel="apple-touch-icon" sizes="114x114" href="/wp-content/uploads/2014/10/W_114x114.jpg" />
+<link rel="apple-touch-icon" sizes="144x144" href="/wp-content/uploads/2014/10/W_144x144.jpg" />
+<link rel="icon" type="image/png" sizes="16x16" href="/wp-content/uploads/2015/09/W-Logo_16X16.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/wp-content/uploads/2015/09/W-Logo_32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/wp-content/uploads/2015/09/W-Logo_96X96.png">
+<link rel="stylesheet" href="/franklin-webfonts/franklingothicfs_condensed_macroman/stylesheet.css" type="text/css" charset="utf-8" />
+<link rel="stylesheet" href="/franklin-webfonts/franklingothicfs_condenseditalic_macroman/stylesheet.css" type="text/css" charset="utf-8" />
+<link rel="stylesheet" href="/franklin-webfonts/franklingothicfs_mediumcondensed_macroman/stylesheet.css" type="text/css" charset="utf-8" />
+<link rel="stylesheet" href="http://usnwc.org/wp-content/themes/usnwc/css/addtohomescreen.css">
+<!--[if lt IE 9]><script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+<script src="<?php echo get_template_directory_uri(); ?>/js/addtohomescreen.min.js?"></script>
+<script src="https://use.fontawesome.com/4945cee666.js"></script>
+<script>
+addToHomescreen({skipFirstVisit:true,maxDisplayCount:1});
+</script>
+<script src="//platform.twitter.com/oct.js" type="text/javascript"></script>
+<script type="text/javascript">
+	twttr.conversion.trackPid('l4lpc');
+</script>
+<noscript>
+<img height="1" width="1" style="display:none;" alt="" src="https://analytics.twitter.com/i/adsct?txn_id=l4lpc&p_id=Twitter" />
+<img height="1" width="1" style="display:none;" alt="" src="//t.co/i/adsct?txn_id=l4lpc&p_id=Twitter" />
+</noscript>
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '236370623380911'); // Insert your pixel ID here.
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=236370623380911&ev=PageView&noscript=1"
+/></noscript>
+<!-- DO NOT MODIFY -->
+<!-- End Facebook Pixel Code -->
+<script type="application/ld+json">
+{ "@context" : "http://schema.org",
+  "@type" : "Organization",
+  "name" : "U.S. National Whitewater Center",
+  "url" : "http://www.usnwc.org",
+  "sameAs" : [ "https://www.linkedin.com/company/220848/" ,
+    "https://www.facebook.com/usnwc/",
+    "https://www.youtube.com/user/theusnwc/",
+    "https://www.instagram.com/usnwc/",
+	“https://vimeo.com/usnwc”]
+}
+</script>
+</head>
+
+<body>
+<div class="base container <?php 
+if(is_category()){
+	$cats="";
+	foreach(get_the_category($post->ID) as $cat){
+		$cats.=$cat->category_nicename." ";
+	}
+	echo $cats;
+} else {
+	echo $post->post_name;
+}
+?>">
+<nav class="mobile">
+</nav>
+<div class="page container">
+<div class="false header"><img class="false img" src="<?php echo get_template_directory_uri()."/images/falselogo.png";?>"></div>
+<header class="page">
+	<?php get_search_form();?>
+	<img class="showsearch" src="<?php echo get_template_directory_uri()."/images/search.png";?>">
+	<img class="showmenu hamburger" src="<?php echo get_template_directory_uri()."/images/hamburger-75x75.png";?>">
+    <nav>
+   		<?php wp_nav_menu( array( 'theme_location' => 'header', 'container' => '' ) ); ?>
+	</nav>
+    <a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img class="logo" src="<?php echo get_template_directory_uri()."/images/web_logo.png";?>" alt="U.S. National Whitewater Center Logo"></a>
+</header>
+<nav class="page breadcrumbs">
+	<li class='breadcrumb-item' id='breadcrumb-0'><a href='<?php echo home_url( '/' );?>'>Home</a></li>
+	<li class='breadcrumb-item'>  &raquo; Search</li>
+</nav>
+<main class="page">
--- search.php
+++ search.php
@@ -6,124 +6,92 @@
 */
 
 get_header('search'); ?>
-<div id="grid">
-
-<?php /* Say hello to the Loop... */
-if ( have_posts() ) : 
 
 
-
-
-
-/* Anything placed in #sort is positioned by jQuery Masonry */ ?>
-<div id="page">
-        
-    <?php while ( have_posts() ) : the_post(); 
-    	
-    	global $my_size, $force_feat_img, $embed_code, $vid_url;
-    	
-        // Gather custom fields
-        //$embed_code = get_post_meta($post->ID, 'soy_vid', true);
-        //$vid_url = get_post_meta($post->ID, 'soy_vid_url', true);
-        $force_feat_img = get_post_meta($post->ID, 'soy_hide_vid', true);
-        $show_title = get_post_meta($post->ID, 'soy_show_title', true);
-        $show_desc = get_post_meta($post->ID, 'soy_show_desc', true);
-        $box_size = get_post_meta($post->ID, 'soy_box_size', true); 
-        
-        if( $box_size == 'Medium (485px)' ){
-            $my_size = 'col3';
-            $embed_size = '495';
-        } else if( $box_size == 'Large (660px)' ){
-            $my_size = 'col4';
-            $embed_size = '670';
-        } else if( $box_size == 'Tiny (135px)' ){
-            $my_size = 'col1';
-            $embed_size = '145';
-        }else{
-            $my_size = 'col2';
-            $embed_size = '320';
+ <?php $post = get_post(45);
+ setup_postdata($post);
+ get_sidebar("banner");
+ wp_reset_postdata();?>
+<?php if(have_posts()){?>
+    <section class="post container <?php 
+        if($args['post_type']&&$args['post_type']==='page'){
+            echo strtolower(preg_replace("/\s+/","-",preg_replace("/&#[0-9]+;/"
+            ,"",get_the_title($args['post_parent']))));
+            if(in_array(array(array(
+                'key' => 'upgrade_check',
+                'value' => 'yes'
+                )
+            ), $args)){
+                echo " upgrades";
+            }
         }
-        
-        /* Check whether content is being displayed
-         * This determines whether a border should be applied
-         * above the postmeta section
-        */
-        if($show_title != 'No'){
-            $content_class = 'has-content';
-        } else if($show_desc != 'No' && $post->post_content){
-            $content_class = 'has-content';
-        }else {
-            $content_class = 'no-content';
+        elseif($args['post_type']&&$args['post_type']==='post'){
+            if($args['cat']){
+                for($i=0;$i<count($args['cat']);$i++){
+                    echo " ".strtolower(preg_replace("/\s+/","-",preg_replace("/&#[0-9]+;/"
+                    ,"",get_the_category_by_ID($args['cat'][$i]))));
+                }
+            }
         }
-        
-        // Assign categories as class names to enable filtering
-        $category_classes = '';
-        
-        foreach( ( get_the_category() ) as $category ) {
-            $category_classes .= $category->category_nicename . ' ';
-        } 
-    ?>
-    
-    <div class="search">
-    
-        
-        <div <?php post_class( 'box-content '.$content_class ) ?>>
-            <?php // Display featured image
-            if ( has_post_thumbnail() ): ?>
-            
-                
-                
-                <?php //if( has_post_format( 'gallery' ) ) get_template_part( 'includes/gallery-list' ); ?>
-                
-            <?php endif; // #has_post_thumbnail() ?>
-            
-            <div class="post-content">
-            
-	            <?php // Display post title ?>
-	           <h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark">
-	                	<?php the_title(); ?>
-	                </a></h2>
-	           
-                <?php // Display post content
-	            
-	            
-	                if( has_excerpt() ){ 
-	                    the_excerpt(); 
-	                } else {
-	                   the_content(__('Continue Reading &rarr;', 'usnwc'));
-	                }
-	                
-	             ?>
-	            
-	             
-            </div><!-- #entry -->
-            
-            <?php // Display post footer ?>
-                <div class="post-footer">
-                <a href="<?php the_permalink(); ?>">
-                   LEARN MORE
-                 </a><!-- link to the post -->  
-                </div>
-            
-            
-        </div><!-- #box-content -->
-        
-    </div><!-- #box -->
-    <?php endwhile; //endif; ?>
+    ?>">
+        <?php while(have_posts()){
+            the_post();
+            $exclude = get_field('exclude');
+            if($exclude && strcmp($exclude,"yes")===0){
+                continue;
+            } 
+            /*
+                * Create a loop to get all the category names 
+                */
+            $category_classes = '';
+            foreach((get_the_category()) as $category){
+                $category_classes .= $category->category_nicename . ' '; 
+            } 
+            /* 
+                * The article will contain all of the content of the queried post
+                * Assign category names and post name to article class
+                */
+            ?>
+            <a name="<?php echo $query->post->post_name;?>"></a>
+            <article class="row <?php echo $category_classes.$query->post->post_name;?>">
+                <?php 
+                /*
+                    * Display the video or post_thumbnail featured image (if any)
+                    */
+                if(has_post_thumbnail()){ 
+                    $img_url=str_replace(home_url(),"",wp_get_attachment_image_src(get_post_thumbnail_id($query->post->ID),array(200,268))[0]);
+                    
+                    if($img_url){
+                        if ( in_array( 'yes', get_field('event_complete') ) ) { ?>
+                            <figure class="featured_image hoverable" style="background-image:url(<?php echo $img_url;?>);">
+                                <img src="/wp-content/uploads/2016/12/EventComplete.png" alt="Event Complete">
+                            </figure>
+                        <?php } elseif ( in_array( 'yes', get_field('sold_out') ) ) { ?>
+                            <figure class="featured_image hoverable" style="background-image:url(<?php echo $img_url;?>);">
+                                <img src="<?php echo get_template_directory_uri()."/images/Sold_Out_Tile.png";?>" alt="Sold Out Tile">
+                            </figure>
+                        <?php } else {?> 
+                            <figure class="featured_image">
+                                <img src="<?php echo $img_url;?>">
+                            </figure>
+                        <?php } 
+                    }
+                } 
+                /*
+                    * Display the title of the post and the content
+                    */
+                ?>
+                <section class="copy">
+                    <header>
+                        <h2><?php the_title(); ?></h2>
+                    </header>
+                    <?php the_excerpt();?>
+                    <?php //display the option to edit the post if logged in?>
+                    <?php edit_post_link(__('Edit this post', 'shaken')); ?>
+                </section>
+            </article>
+        <?php } //end of while ?>
+    </section>
+<?php }?>
 
-
-<?php else : ?>
-	<div class="wrap">
-    <div class="box">
-    	<div class="box-content not-found">
-        	<h2><?php _e('Sorry, nothing was found for your search', 'shaken'); ?></h2>
-            <p><?php _e('Maybe try searching for something different.', 'shaken'); ?></p>
-            <?php get_search_form(); ?>
-        </div>
-    </div>
-     </div><!-- #wrap -->
-    </div><!-- #page -->
-
-<?php endif; ?>
-</div><!-- #grid -->
-<?php get_footer('search'); ?>
+<?php get_footer('page'); ?>
