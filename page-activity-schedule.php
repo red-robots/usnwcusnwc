213aa29f114596e88d17e6567ff4cbd91e980f59
fritzhealy <fre.healy@gmail.com>
2018-06-12 14:01:11 -0400

    adding activity schedule page

---
+++ page-activity-schedule.php
@@ -0,0 +1,69 @@
+ <?php
+/*
+ * Template Name: Activity Schedule
+ */
+
+get_header('page'); ?>
+
+<?php while( have_posts() ) { the_post();
+
+	get_sidebar("banner");?>
+
+	<?php $activities = array(
+		'Whitewater'=>'activity_whitewater',
+		'Flatwater'=>'activity_flatwater',
+		'Climbing'=>'activity_climbing',
+		'Ziplines'=>'activity_ziplines',
+		'Ropes'=>'activity_ropes',
+		'Jumps'=>'activity_jumps',
+		'Trails'=>'activity_trails'
+	);?>
+	<section class="post container activity-schedule">
+		<header>
+			<h1><?php the_title();?></h1>
+		</header>
+		<?php $date_notice = get_field("date_notice");
+		if($date_notice){?>
+			<h2><?php echo $date_notice;?></h2>
+		<?php }//endif?>
+		<ul class="activity-schedule-list">
+			<?php /* loop for activity access section */
+			foreach($activities as $key => $activity){?>
+				<li class="top-level-item">
+					<span class="title"><?php echo $key;?></span>
+					<ul>
+						<?php $rows = get_field($activity);
+						if($rows){
+							foreach($rows as $row) {
+								$closed = $row['closed'];
+								$start_time = $row['start_time'];
+								$end_time = $row['end_time'];
+								$title = $row['title'];?>
+								<?php if($title){?>
+									<li class="<?php $now = new DateTime();
+									if(!empty($closed)||
+										((new DateTime($start_time))->getTimestamp())>$now->getTimestamp()||
+										((new DateTime($end_time))->getTimestamp())<$now->getTimestamp()) 
+										echo "closed";?>">
+										<span class="title"><?php echo $title;?></span>
+										<?php if($start_time||$end_time){?>
+											<span class="time"><?php if($start_time){
+												echo $start_time;
+											}
+											echo "-";
+											if($end_time){
+												echo $end_time;
+											}?></span>
+										<?php }//endif?>
+									</li>
+								<?php }//endif?>
+							<?php }//end foreach 
+						}//endif?>
+					</ul>
+				</li>
+			<?php } //end for foreach ?>
+		</ul>	
+		<?php the_content();?>
+	</section>
+<?php }//endwhile
+ get_footer('page'); ?>
